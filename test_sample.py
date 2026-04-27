209bf1b6097607bd3d12cbbb73d139014156d462
nogueirinhaa1 <nogueirinhaa1@gmail.com>
2026-04-27 11:30:48 -0300

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,2 +1,18 @@
 def test_example():
     assert True
+
+def test_soma():
+    resultado = 2 + 3
+    assert resultado == 5
+
+def test_string_contem():
+    texto = "devops"
+    assert "ops" in texto
+
+def test_lista_vazia():
+    lista = []
+    assert len(lista) == 0
+
+def test_dict_chave():
+    d = {"nome": "Cristhian"}
+    assert "nome" in d
